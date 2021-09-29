ec41cf4ee4ff2c2db26f0d18eb7ab296c00c42c3
Luis Moromisato <moromisato.luis@gmail.com>
2021-09-28 23:21:30 -0300

    added jwt signin key added first callback

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,14 +1,36 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers';
 
+import { query as q } from 'faunadb';
+import { fauna } from '../../../services/fauna';
+
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       scope: 'read:user'
     }),
-    // ...add more providers here
   ],
+  jwt: {
+    signingKey: process.env.SIGNING_KEY,
+  },
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email }}
+          )
+        )
+        return true
+      } catch (error) {
+        console.log(error)
+        return false
+      }
+    }
+  }
 })
