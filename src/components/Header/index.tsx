452b0af98453c8b874bffd655ef1d0c1b92f21d4
moromisato <moromisato.luis@gmail.com>
2022-04-16 20:54:50 -0300

    add header test

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
-import { ActiveLink } from './ActiveLink';
+import { ActiveLink } from '../ActiveLink/index';
 
 export function Header() {
 
