147e354d15c23fd388908f548504bccc5dcdf6e2
moromisato <moromisato.luis@gmail.com>
2022-04-17 00:41:34 -0300

    update active link tests

--- src/components/ActiveLink/ActiveLink.spec.tsx
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -1,18 +1,17 @@
 import { render } from '@testing-library/react';
 import { ActiveLink } from '.';
+import { useRouter } from 'next/router';
+import { mocked } from 'jest-mock';
 
-jest.mock('next/router', () => {
-  return {
-    useRouter() {
-      return {
-        asPath: '/'
-      }
-    }
-  }
-});
+jest.mock('next/router');
 
 describe('ActiveLink', () => {
   it('renders correctly', () => {
+    const useRouterMocked = mocked(useRouter);
+    useRouterMocked.mockReturnValueOnce({
+      asPath: '/'   
+    } as any)
+
     const { getByText } = render(
       <ActiveLink href='/' activeClassName="active">
         <a>Home</a>
@@ -23,14 +22,33 @@ describe('ActiveLink', () => {
   });
 
   it('adds active class if the link is currently active', () => {
+    const useRouterMocked = mocked(useRouter);
+    useRouterMocked.mockReturnValueOnce({
+      asPath: '/'   
+    } as any)
+
     const { getByText } = render(
-      <ActiveLink href='/' activeClassName="active">
+      <ActiveLink href='/' activeClassName='active'>
         <a>Home</a>
       </ActiveLink>
     )
   
     expect(getByText('Home')).toHaveClass('active');
   })
-  
+
+  it('should not add class if the link is not active', () => {
+    const useRouterMocked = mocked(useRouter);
+    useRouterMocked.mockReturnValueOnce({
+      asPath: '/some'
+    } as any)
+    
+    const { getByText } = render(
+      <ActiveLink href='/' activeClassName='active'>
+        <a>Home</a>
+      </ActiveLink>
+    )
+
+    expect(getByText('Home')).not.toHaveClass('active');
+  })
 })
 
